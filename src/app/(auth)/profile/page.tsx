ca4f729d07c9149ac8311301dd87c1383d218ecf
dev-nohyj <goky1129@gmail.com>
2023-08-22 16:37:19 +0900

    auth check

--- src/app/(auth)/profile/page.tsx
+++ src/app/(auth)/profile/page.tsx
@@ -1,8 +1,14 @@
 import Main from '@/app/components/profile/Main';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 interface Props {}
 
 const ProfilePage = ({}: Props) => {
+    const cookie = cookies().get(process.env.COOKIE as string);
+    if (!cookie) {
+        redirect('/');
+    }
     return (
         <main>
             <Main />
