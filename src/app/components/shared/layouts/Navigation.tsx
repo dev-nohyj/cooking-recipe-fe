3ea3f5c334ef7975705276f05e980d4ba63b9cea
dev-nohyj <goky1129@gmail.com>
2023-09-06 14:21:09 +0900

    Add/logout cache

--- src/app/components/shared/layouts/Navigation.tsx
+++ src/app/components/shared/layouts/Navigation.tsx
@@ -39,6 +39,7 @@ const Navigation = ({}: Props) => {
             alert('시스템 에러가 발생했습니다.\n잠시 후 다시 시도해 주세요.');
         },
         onSuccess: () => {
+            cache.removeQueries();
             cache.setQueryData<TGetProfileData>(GetProfileQueryKey(), (prev) => {
                 if (prev) {
                     const newData = produce(prev, (draft) => {
